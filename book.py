521b244489b1488d0ff4c819b521511b308e251e
Eugene <chupa14142@gmail.com>
2024-03-14 13:04:30 +0200

    add class Review for task 2

--- book.py
+++ book.py
@@ -9,14 +9,25 @@ class Book:
         self.book_name = book_name
         self.published_year = published_year
         self.genre = genre
+        self.reviews = []
 
     def __str__(self):
         return (f"Author: {self.author}, Book: {self.book_name},"
-                f" Published year: {self.published_year}, Genre: {self.genre}")
+                f" Published year: {self.published_year},"
+                f" Genre: {self.genre}, reviews: {self.reviews}")
 
     def __repr__(self):
         return (f"Author: {self.author}, Book: {self.book_name},"
-                f" Published year: {self.published_year}, Genre: {self.genre}")
+                f" Published year: {self.published_year},"
+                f" Genre: {self.genre}, reviews: {self.reviews}")
+
+
+class Review:
+    """Review for task 2"""
+
+    @staticmethod
+    def add_review_to_book(book: Book, text_review) -> None:
+        book.reviews.append(text_review)
 
 
 book1 = Book("eugene", "python starter", 2024, "science")
@@ -24,6 +35,11 @@ book2 = Book("andrew", "python essential", 1991, "science")
 book3 = Book("lola", "python advanced", 2017, "science")
 
 books = [book1, book2, book3]
+
+my_review_for_book1 = Review.add_review_to_book(book1, "very good book")
+my_review_for_book2 = Review.add_review_to_book(book2, "fascinating")
+my_review_for_book3 = Review.add_review_to_book(book3, "very interesting")
+my_review_for_book3_1 = Review.add_review_to_book(book3, "great book")
 
 print(book1, book2, book3, end="\n")
 print(books)
